ddd2efe36a20c2b338ea09641b5e651442b6e9b5
rohit8044 <trickcric2@gmail.com>
2026-05-13 20:33:28 -0700

    Updated responsive table design

--- src/Sidebar_items/User_Management/Add_Days.jsx
+++ src/Sidebar_items/User_Management/Add_Days.jsx
@@ -1,7 +1,70 @@
-export default function Add_Day(){
-    return(
+import './Add_Days.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+export default function Add_Day() {
+
+    const [data, setdata] = useState([])
+
+    useEffect(() => {
+
+        axios.get('https://harry80442s.onrender.com/Api/all')
+            .then((response) => {
+
+                console.log(response.data)
+
+                setdata(response.data)
+
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+    }, [])
+
+    return (
         <>
-            <h1>Add Day</h1>
+            <div className='Daycontainer'>
+
+                <div className='table-items'>
+
+                    <table border="1">
+
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th>Department</th>
+                                <th>Salary</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+
+                            {data.map((res, index) => {
+                                return (
+                                    <tr key={index}>
+
+                                        <td>{res.id}</td>
+                                        <td>{res.name}</td>
+                                        <td>{res.email}</td>
+                                        <td>{res.role}</td>
+                                        <td>{res.department}</td>
+                                        <td>{res.salary}</td>
+
+                                    </tr>
+                                )
+                            })}
+ 
+                        </tbody>
+
+                    </table>
+
+                </div>
+
+            </div>
         </>
     )
 }
